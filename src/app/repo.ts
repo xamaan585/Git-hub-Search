3f69e2b022a0a59eddab12e0a0431168e6ac3594
Abdirahman <dubowhamaan@gmail.com>
2022-01-19 11:20:52 +0300

    adding repo.ts

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,13 @@
 export class Repo {
+    constructor(
+        public html_url:string,
+        public clone_url:string,
+        public name:string,
+        public description:string,
+        public language:string,
+        public created_at:Date,
+        public updated_at :Date
+    ){
+
+    }
 }
