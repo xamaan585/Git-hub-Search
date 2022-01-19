8f35bf7472aa7e9861aceed71ac8c9524a3877f6
Abdirahman <dubowhamaan@gmail.com>
2022-01-19 14:26:18 +0300

    adding git routing module.ts

--- src/app/git-routing/git-routing.module.ts
+++ src/app/git-routing/git-routing.module.ts
@@ -1,12 +1,23 @@
+import { SearchDisplayComponent } from '../search-display/search-display.component'; 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchFormComponent } from 'src/app/search-form/search-form.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
 
-
+const routes: Routes = [
+  { path: 'search', component: SearchFormComponent},
+  {path:'',redirectTo:'search', pathMatch: 'full'},
+  { path: 'display', component: SearchDisplayComponent},
+  { path:'**', component:NotFoundComponent},
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class GitRoutingModule { }
