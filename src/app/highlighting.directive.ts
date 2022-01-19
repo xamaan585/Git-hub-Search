586323de09fe6ab9730d62d54cc43905e5367616
Abdirahman <dubowhamaan@gmail.com>
2022-01-19 11:27:11 +0300

    adding highlighting directive.ts

--- src/app/highlighting.directive.ts
+++ src/app/highlighting.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlighting]'
 })
 export class HighlightingDirective {
 
-  constructor() { }
-
+  constructor(private elem:ElementRef) { }
+  @HostListener('mouseenter') onMouseOver(){
+    this.changeColor('purple');
+  }
+  @HostListener('mouseleave') onMouseOut() {
+    this.changeColor(null); 
+  }
+  private changeColor(textColor: any) {
+    this.elem.nativeElement.style.color = textColor;
+  }
 }
