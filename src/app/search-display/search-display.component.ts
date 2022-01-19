1dc93f9a2ab953d2f0af3064b96cc044ee287bc6
Abdirahman <dubowhamaan@gmail.com>
2022-01-19 16:37:45 +0300

    adding search-display component.ts

--- src/app/search-display/search-display.component.ts
+++ src/app/search-display/search-display.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../services/user.service';
+import { User } from'./../user';
+import { Repo } from '../repo';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-search-display',
   templateUrl: './search-display.component.html',
   styleUrls: ['./search-display.component.css']
 })
 export class SearchDisplayComponent implements OnInit {
+  user!: User;
+  isComplete = true;
+  repository:Repo[]=[];
+  username!:string;
+  userService!: UserService;
 
-  constructor() { }
+  constructor(private GitUserService:UserService,private router:Router,private location:Location) { 
+    this.userService=GitUserService
+  }
 
   ngOnInit(): void {
+    this.userService.getUserData(this.username);
+    this.user = this.userService.repositories;
+
+    this.userService.getRepo(this.username);
+    this.repository = this.userService.repositories;
+  }
+  back(){
+    this.router.navigate(['search']);
   }
 
 }
