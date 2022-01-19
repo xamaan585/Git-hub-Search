8b195e8dfe871d77c32920e6aefbed22b3d7bb1b
Abdirahman <dubowhamaan@gmail.com>
2022-01-19 11:43:35 +0300

    addin pipes

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,12 +1,39 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe } from '@angular/core';
+import { TimeAgoPipe } from 'time-ago-pipe';
 
 @Pipe({
-  name: 'dateCount'
+  name: 'dateCount',
+  pure: false
 })
-export class DateCountPipe implements PipeTransform {
+export class DateCountPipe extends TimeAgoPipe {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  timeAgoPipe(value: any): any {
+    return super.transform(value);
   }
+
+  if (value: string | number | Date) {
+    const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+   
+    const intervals:any = {
+        'year': 31536000,
+        'month': 2592000,
+        'week': 604800,
+        'day': 86400,
+        'hour': 3600,
+        'minute': 60,
+        'second': 1
+    };
+    let counter;
+    for (const i in intervals) {
+        counter = Math.floor(seconds / intervals[i]);
+        if (counter > 0)
+            if (counter === 1) {
+               counter + ' ' + i + ' ago'; // singular 
+            } else {
+              counter + ' ' + i + 's ago'; // plural (2 days ago)
+            }
+    }
+}
+
 
 }
