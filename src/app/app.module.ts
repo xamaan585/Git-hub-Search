b479f47b3924b368084582889ac6aad20593d8b7
Abdirahman <dubowhamaan@gmail.com>
2022-01-19 12:55:45 +0300

    adding service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GitRoutingModule } from './git-routing/git-routing.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightingDirective } from './highlighting.directive';
@@ -22,7 +25,11 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    GitRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,97 @@
+import { Repo } from './../repo';
 import { Injectable } from '@angular/core';
+import { User } from '../user';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {environment } from '../../environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  user!: User;
+  repository: Repo;
+  repositories:any= []
+  UserInfo :any = []
+  url= environment.ApiUrl;
+
+  constructor(private httpClient:HttpClient,private router:Router) {
+    this.user = new User("","","","",0,new Date(),new Date());
+    this.repository = new Repo("","","","","",new Date(),new Date())
+  }
+
+  
+ getUserData(username:string){
+
+   this.repositories.length = 0;
+   interface ApiResponse{
+    name: string, 
+     avatar_url: any, 
+     html_url: string, 
+     public_repos: number,
+     created_at: Date,
+     updated_at:Date
+   }
+
+   let promise = new Promise<ApiResponse | void>((resolve,reject)=>{
+     this.httpClient.get < any > (this.url + username, {
+       headers: new HttpHeaders({
+         'Authorization': 'Bearer ' + this.apiUrl,
+
+       })
+     }).toPromise()
+     .then(response=>{
+      
+       console.log("=====",username);
+       this.user.name= response.name;
+       this.user.login= response.login;
+       this.user.avatar_url= response.avatar_url;
+       this.user.html_url= response.html_url;
+       this.user.public_repos= response.public_repos;
+       this.user.created_at= response.created_at;
+       this.user.updated_at= response.updated_at;
+
+       resolve()
+     },
+     error=>{
+      console.log(error.message);
+      
+       reject(error)
+     })
+
+   })
+   
+   return promise;
+ }
+ getRepo(username:String){
+    console.log("=====",username);
+
+   let promise = new Promise<void>((resolve,reject)=>{
+     this.httpClient.get < any > (this.url + username + "/repos", {
+       headers: new HttpHeaders({
+         'Authorization': 'Bearer ' + this.apiUrl
+       })
+     }).toPromise()
+     .then(response=>{
+       console.log("&&&&&&&&&&&&",response);
+       this.repository= response
+      
+       resolve(response)
+       for(var i=0; i<response.length; i++)
+           {
+           
+             this.UserInfo = new Repo(response[i].html_url,response[i].clone_url,response[i].name,response[i].description,response[i].language,response[i].created_at,response[i].updated_at);
+             this.repositories.push(this.UserInfo);
+           }
+     },
+     error=>{
+      console.log("^^^^^^^^^^^^^^^^^",error);
+
+       reject(error)
+     })
+    
+   })
+   return promise;
+  }
 }
