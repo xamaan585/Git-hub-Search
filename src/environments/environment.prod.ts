846364d3fd24e361fa84522fa75cac5cc33bcccf
Abdirahman <dubowhamaan@gmail.com>
2022-01-25 15:00:40 +0300

    final commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true,
+  url:"https://api.github.com/users/",
+  token: '?access_token= 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',
+  personalAccessToken:'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'
 };
